8634739ba847f6555d7629114a97f1e71e641d03
GR-NAYAK-0123 <rajanayak7889@gmail.com>
2025-07-17 03:57:15 +0000

    Learn about Data types

---
+++ 01_basic/02_dataTypes.js
@@ -0,0 +1,34 @@
+"use strict"  //It will treat all JavaScript code as newer version
+
+// alert(3+5);  //It gives error bcz here we are using nodejs not browser
+
+console.log(5 + 5);             //We need to take care about the readability of the code
+
+let state = null;
+let age = 23;
+let flag = true;
+
+
+/*
+        Data Types
+    ----------------------
+        1. number
+
+        2. bigint
+
+        3. string  ("")
+
+        4. boolean  (true / false)
+
+        5. null  (standalone value)
+
+        6. undefined
+
+        7. symbol  (Unique)
+*/
+
+console.log(typeof null);   //Object
+console.log(typeof undefined);  //undefined
+console.log(typeof flag);
+
+
