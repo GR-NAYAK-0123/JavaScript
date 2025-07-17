16b209bfd7e94bb0e1e4a0d312e57309125bcfd1
GR-NAYAK-0123 <rajanayak7889@gmail.com>
2025-07-17 06:00:23 +0000

    Learn about conversion

---
+++ 01_basic/04_comparision.js
@@ -0,0 +1,23 @@
+// console.log(2 > 3);
+// console.log(2 == 3);
+// console.log(2 >= 3);
+// console.log(2 < 3);
+// console.log(2 <= 3);
+
+// console.log("2" > 1);
+// console.log("03" >= 2);
+
+
+
+// console.log(null > 0);
+// console.log(null == 0);  // In JS this (==) operator works differently
+// console.log(null >= 0);
+
+// console.log(undefined > 0);
+// console.log(undefined == 0);  // Always try to avoid this kind of conversion
+// console.log(undefined >= 0);
+
+/* (===) -> It checks strictly means it doesn't typecast any data 
+          type if both values have same data type then it checks otherwise gives false
+*/
+console.log("2" === 1);
