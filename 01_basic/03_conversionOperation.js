c28c8434c3307565474ebbbc0f89dd5ed5228692
GR-NAYAK-0123 <rajanayak7889@gmail.com>
2025-07-17 05:23:56 +0000

    Add some operations details

--- 01_basic/03_conversionOperation.js
+++ 01_basic/03_conversionOperation.js
@@ -26,11 +26,53 @@ let priceIntoBoolean = Boolean(price);
 
 let anyNumber = undefined;
 let stringNumber = String(anyNumber);
-console.log(stringNumber);
-console.log(typeof stringNumber);
+// console.log(stringNumber);
+// console.log(typeof stringNumber);
 
 // Here is the conversion from any datatype to String
 // 12 -> 12
 // null -> null
 // undefined -> undefined
+
+//####################################### Operations ####################################################
+
+let number =7;
+let negativeNumber = -number;
+
+// console.log(negativeNumber);
+
+// console.log(2+2);
+// console.log(2-2);
+// console.log(2*2);
+// console.log(2**2);   // It gives power value 
+// console.log(2/2);
+// console.log(2%2);
+
+let str1 = "Hii "
+let str2 = "Raja"
+let str3 = str1 + str2;
+// console.log(str3);
+
+// console.log(1 + "8");
+// console.log("1" + 8);
+// console.log(1 + 4 + "8");
+// console.log("3" + 2);
+
+/*
+    1) Whenever you want to convert this above types if you write number first then upto the number it convert to number then it can add with string
+
+    2) If you are write string first then after the string all are converted into string by default
+*/
+
+// console.log(+true);
+
+let num1, num2, num3;
+num1 = num2 = num3 = 9 + 7;
+// console.log(num1);
+
+let a = 20;
+console.log(a++);  //Post-increment  First access then increase
+console.log(++a);  //pre-increment  First increase then access
+console.log(a);
+
 
