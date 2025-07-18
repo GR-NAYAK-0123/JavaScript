6f0819d7dc65ce746b8fded4f44025b7b73b7d77
GR-NAYAK-0123 <rajanayak7889@gmail.com>
2025-07-18 05:42:16 +0000

    numbers and maths

---
+++ 01_basic/07_numbers_math.js
@@ -0,0 +1,38 @@
+const value = 89;
+
+const score = new Number(100);
+
+// console.log(score);
+
+// console.log(score.toString().length);
+// console.log(score.toFixed(2));
+
+const balance = 1000000;
+// console.log(balance.toLocaleString('en-IN'));   //It represents indian type
+// console.log(balance.toLocaleString());          //It represents USA type
+
+
+
+// =========================================== Maths =================================================
+
+// console.log(Math.abs(-3));
+// console.log(Math.pow(4,3));
+// console.log(Math.ceil(6.2));
+// console.log(Math.ceil(6.2));    //It converts to its upper next value 
+// console.log(Math.floor(6.2));   //It remains same even if you give maximum decimal value
+
+// console.log(Math.max(3,6,3,8));
+
+console.log(Math.random());
+console.log((Math.random() * 10) + 1);
+console.log(Math.floor((Math.random() * 10) + 1));
+
+const max = 20;
+const min = 10;
+
+console.log(Math.floor(Math.random() * (max - min + 1)) + min);
+
+
+
+
+
